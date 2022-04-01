8eb000559c9e2fccb626aecd36ad746d6b27747b
Daniel Wijaya <dwjy27@gmail.com>
2022-04-01 14:23:46 +0700

    Refactor home page

--- home.php
+++ home.php
@@ -5,65 +5,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Home</title>
-    <style>
-        *,
-        *::after,
-        *::before {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-
-        body {
-            background: #CAD1FF;
-        }
-
-        nav {
-            background: #F9FFCA;
-            display: flex;
-            justify-content: space-between;
-            padding: 1rem;
-            font-size: 1.3rem;
-        }
-
-        .navmenu {
-            display: flex;
-            gap: 2rem;
-        }
-
-        .logout {
-            margin-left: 5rem;
-        }
-
-        .container {
-            display: flex;
-            justify-content: center;
-            padding-block: 10rem;
-            font-size: 2rem;
-        }
-
-        .current {
-            font-weight: bold;
-        }
-
-        a {
-            text-decoration: none;
-            color: black;
-        }
-    </style>
 </head>
 
-<body>
-    <nav>
+<body class="bg-purple">
+    <nav class="flex">
         <h2>Aplikasi pengelolaan Keungaan</h2>
-        <div class="navmenu">
+        <div class="flex">
             <a class="current" href="home.php">Home</a>
             <a href="profile.php">Profile</a>
             <a class="logout" href="processLogout.php">Logout</a>
         </div>
     </nav>
-    <div class="container">
+    <div class="flex flex-center message-container">
         <?php
         session_start();
         $name = implode(' ', [$_SESSION['frontName'], $_SESSION['middleName'], $_SESSION['lastName']]);
