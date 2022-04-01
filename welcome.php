d03402d4fccf1c16463cca6ecb372c043162b3a0
Daniel Wijaya <dwjy27@gmail.com>
2022-04-01 12:21:30 +0700

    Refactor welcome page and add external style.css

--- welcome.php
+++ welcome.php
@@ -5,60 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Welcome</title>
-    <style>
-        *,
-        *::after,
-        *::before {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-
-        body {
-            padding: 3rem;
-            background: #E5EDEB;
-            text-align: center;
-        }
-
-        .container {
-            margin: 5rem;
-        }
-
-        .btn-container {
-            margin: 3rem;
-            display: flex;
-            gap: 3rem;
-            justify-content: center;
-        }
-
-        .btn {
-            padding: 1em;
-            font-size: 2rem;
-            border: none;
-            cursor: pointer;
-        }
-
-        .login-btn {
-            background: #99D6ED;
-        }
-
-        .register-btn {
-            background: #C6ED99;
-        }
-
-        a {
-            text-decoration: none;
-            color: black;
-        }
-    </style>
 </head>
 
-<body>
-    <h2>Aplikasi pengelolaan Keungaan</h2>
-    <div class="container">
+<body class="bg-gray">
+    <div class="container flex-column flex-center">
+        <h2 class="title">Aplikasi pengelolaan Keungaan</h2>
         <h1>Selamat Datang di Aplikasi Pengelolaan Keuangan</h1>
-        <div class="btn-container">
+        <div class="container flex-center btn-container">
             <a href="login.php">
                 <button id="login" class="btn login-btn">
                     Login
