8d676b2f6e4b3d1d8491a7de228a5275e1b7535c
Daniel Wijaya <dwjy27@gmail.com>
2022-04-01 12:56:54 +0700

    Tidying up style.css

--- welcome.php
+++ welcome.php
@@ -10,10 +10,10 @@
 </head>
 
 <body class="bg-gray">
-    <div class="container flex-column flex-center">
+    <div class="flex flex-column flex-center content-container">
         <h2 class="title">Aplikasi pengelolaan Keungaan</h2>
         <h1>Selamat Datang di Aplikasi Pengelolaan Keuangan</h1>
-        <div class="container flex-center btn-container">
+        <div class="flex flex-center btn-container">
             <a href="login.php">
                 <button id="login" class="btn login-btn">
                     Login
